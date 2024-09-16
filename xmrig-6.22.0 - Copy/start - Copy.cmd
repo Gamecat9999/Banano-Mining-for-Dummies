c4cdf3376a3065e645155e8dab4334b97c06654b
Gamecat999 <justjames_b@outlook.com>
2024-09-16 10:41:14 -0600

    Add files via upload

---
+++ xmrig-6.22.0 - Copy/start - Copy.cmd
@@ -0,0 +1,2 @@
+xmrig.exe -o xmrig.nanswap.com:3333 -a rx -k -u ban_3t4887towijyq37e838hniui7yakxwsp3gutt415scbrtif4536ia8wzqjus -p x
+pause
---
+++ xmrig-6.22.0 - Copy/start.cmd
@@ -0,0 +1,2 @@
+xmrig.exe -o xmrig.nanswap.com:3333 -a rx -k -u nano_16kq38woyqixpc67qcjcdxb8ebajzpegjaiiefu8yu1rqoub4b7cwy364m4k -p x
+pause
